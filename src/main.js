a531976984d93d89d84a087f59799a3b7e39133f
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2018-06-10 15:07:51 +0700

    Update 'main.js' configuration

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import axios from 'axios'
+import {routes} from './routes'
+
+Vue.use(VueRouter);
+
+axios.defaults.baseURL = 'https://arga-ga-backend.firebaseio.com/';
+axios.defaults.headers.get['Accepts'] = 'application/json';
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
