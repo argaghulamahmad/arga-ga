b6febe854d09ea8a1f1a38777aa43a04b6d85599
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2018-06-30 21:10:07 +0700

    Imporove app performance Update routes mechanism to improve performance.

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,39 @@
 import aAboutMe from './components/sections/aAboutMe';
-import aAwards from './components/sections/aAchievements';
-import aEducation from './components/sections/aEducation';
-import aExperience from './components/sections/aExperience';
-import aInterests from './components/sections/aInterests';
-import aSkills from './components/sections/aSkills';
-import aProjects from './components/sections/aProjects'
-import aAuth from './components/partials/aAuth'
+
+const aAwards = resolve => {
+  require.ensure(['./components/sections/aAchievements'], () => {
+    resolve(require('./components/sections/aAchievements'))
+  })
+};
+const aEducation = resolve => {
+  require.ensure(['./components/sections/aEducation'], () => {
+    resolve(require('./components/sections/aEducation'))
+  })
+};
+const aExperience = resolve => {
+  require.ensure(['./components/sections/aExperience'], () => {
+    resolve(require('./components/sections/aExperience'))
+  })
+};
+const aInterests = resolve => {
+  require.ensure(['./components/sections/aInterests'], () => {
+    resolve(require('./components/sections/aInterests'))
+  })
+};
+const aSkills = resolve => {
+  require.ensure(['./components/sections/aSkills'], () => {
+    resolve(require('./components/sections/aSkills'))
+  })
+};const aProjects = resolve => {
+  require.ensure(['./components/sections/aProjects'], () => {
+    resolve(require('./components/sections/aProjects'))
+  })
+};const aAuth = resolve => {
+  require.ensure(['./components/partials/aAuth'], () => {
+    resolve(require('./components/partials/aAuth'))
+  })
+};
+
 
 export const routes = [
   {
