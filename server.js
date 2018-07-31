9e7066c8336f61fc5984cabf03feb3bc09639c8a
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2018-07-31 11:40:49 +0700

    Improve web performance. Apply dynamic gzip. Enable text compression, using express js compression middleware.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
+var compression = require('compression'); //import to express app
 
+app.use(compression());//add this as the 1st middleware
 app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req, res) {
   res.sendFile(__dirname + "/dist/index.html");
